27f9f31bce97de787f1576ddd92cf739f9286faa
Rutu83 <rathodrutu1996@gmail.com>
2024-07-14 15:53:04 +0530

    All sum up

--- app/src/main/java/com/threebrains/odoolibrary/AddLibrarianActivity.java
+++ app/src/main/java/com/threebrains/odoolibrary/AddLibrarianActivity.java
@@ -1,14 +1,36 @@
 package com.threebrains.odoolibrary;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddLibrarianActivity extends AppCompatActivity {
+    private TextInputEditText etUsername, etEmail, etPassword, etConfirmPassword;
+    private Button btnAddLibrarian;
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +42,87 @@ public class AddLibrarianActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        etUsername = findViewById(R.id.et_username);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        etConfirmPassword = findViewById(R.id.et_confirm_password);
+        btnAddLibrarian = findViewById(R.id.btn_sign_up);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        btnAddLibrarian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString().trim();
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                String confirmPassword = etConfirmPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(username)) {
+                    etUsername.setError("Name is required.");
+                    etUsername.requestFocus();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(email)) {
+                    etEmail.setError("Email is required.");
+                    etEmail.requestFocus();
+                    return;
+                }
+
+                if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    etEmail.setError("Please enter a valid email address.");
+                    etEmail.requestFocus();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    etPassword.setError("Password is required.");
+                    etPassword.requestFocus();
+                    return;
+                }
+
+                if (password.length() < 8) {
+                    etPassword.setError("Password must be at least 8 characters long.");
+                    etPassword.requestFocus();
+                    return;
+                }
+
+                if (!password.equals(confirmPassword)) {
+                    etConfirmPassword.setError("Passwords do not match.");
+                    etConfirmPassword.requestFocus();
+                    return;
+                }
+
+                addLibrarianToFirestore(username, email);
+            }
+        });
+
+    }
+
+    private void addLibrarianToFirestore(String username, String email) {
+        Map<String, Object> librarian = new HashMap<>();
+        librarian.put("username", username);
+        librarian.put("email", email);
+        librarian.put("role", "Librarian");
+
+        db.collection("users")
+                .add(librarian)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        if (task.isComplete()){
+                            Toast.makeText(AddLibrarianActivity.this, "Librarian added", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(AddLibrarianActivity.this, "Error adding librarian: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.w("AddLibrarianActivity", "Error adding document", e);
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/threebrains/odoolibrary/fragments/ProfileFragment.java
+++ app/src/main/java/com/threebrains/odoolibrary/fragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.threebrains.odoolibrary.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.Firebase;
@@ -17,12 +20,14 @@ import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.threebrains.odoolibrary.R;
+import com.threebrains.odoolibrary.auth.Login;
 
 public class ProfileFragment extends Fragment {
 
     FirebaseAuth firebaseAuth;
     FirebaseFirestore firestore;
-    String Email,UserName;
+    TextView Email, UserName;
+    LinearLayout Logout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,13 +37,26 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View fragProfile = inflater.inflate(R.layout.fragment_profile, container, false);
+        View fragProfile = inflater.inflate(R.layout.layout_profile, container, false);
         firestore = FirebaseFirestore.getInstance();
         firebaseAuth = FirebaseAuth.getInstance();
+        UserName = fragProfile.findViewById(R.id.tv_user_name);
+        Email = fragProfile.findViewById(R.id.tv_email);
+        Logout = fragProfile.findViewById(R.id.ll_logout);
+        fetchProfile();
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                startActivity(new Intent(getActivity(), Login.class));
+            }
+        });
 
         return fragProfile;
     }
-    public void fetchProfile(){
+
+
+    public void fetchProfile() {
         FirebaseUser currentUser = firebaseAuth.getCurrentUser();
         if (currentUser != null) {
             String userId = currentUser.getUid();
@@ -47,8 +65,8 @@ public class ProfileFragment extends Fragment {
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
-//                        tvUsername.setText(document.getString("username"));
-//                        tvEmail.setText(document.getString("email"));
+                        UserName.setText(document.getString("username"));
+                        Email.setText(document.getString("email"));
                     } else {
                         Toast.makeText(getActivity(), "User data not found", Toast.LENGTH_SHORT).show();
                     }
@@ -59,4 +77,4 @@ public class ProfileFragment extends Fragment {
         }
     }
 
-    }
+}
