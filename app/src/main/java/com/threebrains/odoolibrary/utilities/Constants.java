917e51db14437a8f871c33c155cb4df2368b6f1c
Rutu83 <rathodrutu1996@gmail.com>
2024-07-14 11:18:20 +0530

    Features as follow Splash screen Login Signup

---
+++ app/src/main/java/com/threebrains/odoolibrary/utilities/Constants.java
@@ -0,0 +1,13 @@
+package com.threebrains.odoolibrary.utilities;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
+public class Constants {
+    public static String UID = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+    public static String  ROLE="";
+
+}
