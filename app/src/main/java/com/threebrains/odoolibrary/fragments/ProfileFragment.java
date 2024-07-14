9f09bb5378e51369de49fbc0354bbab81f44c2d5
Rutu83 <rathodrutu1996@gmail.com>
2024-07-14 14:51:32 +0530

    All sum up

--- app/src/main/java/com/threebrains/odoolibrary/fragments/ProfileFragment.java
+++ app/src/main/java/com/threebrains/odoolibrary/fragments/ProfileFragment.java
@@ -7,10 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.threebrains.odoolibrary.R;
 
 public class ProfileFragment extends Fragment {
+
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firestore;
+    String Email,UserName;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,30 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View fragProfile = inflater.inflate(R.layout.fragment_profile, container, false);
+        firestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
         return fragProfile;
     }
-}
+    public void fetchProfile(){
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+
+            firestore.collection("users").document(userId).get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+//                        tvUsername.setText(document.getString("username"));
+//                        tvEmail.setText(document.getString("email"));
+                    } else {
+                        Toast.makeText(getActivity(), "User data not found", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getActivity(), "Error getting user data", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    }
